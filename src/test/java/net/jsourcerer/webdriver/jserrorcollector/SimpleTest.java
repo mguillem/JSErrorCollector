928a35f937a9465e2574998f397783dfab20bfaa
Marc Guillemot <mguillemot@yahoo.fr>
2012-11-09 16:26:41 +0100

    adapted expected error messages to FF16

--- src/test/java/net/jsourcerer/webdriver/jserrorcollector/SimpleTest.java
+++ src/test/java/net/jsourcerer/webdriver/jserrorcollector/SimpleTest.java
@@ -21,18 +21,18 @@ import static org.junit.Assert.assertEquals;
  */
 public class SimpleTest {
 	private final String urlSimpleHtml = getResource("simple.html");
-	private final JavaScriptError errorSimpleHtml = new JavaScriptError("null has no properties", urlSimpleHtml, 9);
+	private final JavaScriptError errorSimpleHtml = new JavaScriptError("TypeError: null has no properties", urlSimpleHtml, 9);
 	
 	private final String urlWithNestedFrameHtml = getResource("withNestedFrame.html");
-	private final JavaScriptError errorWithNestedFrameHtml = new JavaScriptError("\"foo\".notHere is not a function", urlWithNestedFrameHtml, 7);
+	private final JavaScriptError errorWithNestedFrameHtml = new JavaScriptError("TypeError: \"foo\".notHere is not a function", urlWithNestedFrameHtml, 7);
 
 	private final String urlWithPopupHtml = getResource("withPopup.html");
 	private final String urlPopupHtml = getResource("popup.html");
-	private final JavaScriptError errorPopupHtml = new JavaScriptError("error is not defined", urlPopupHtml, 5);
+	private final JavaScriptError errorPopupHtml = new JavaScriptError("ReferenceError: error is not defined", urlPopupHtml, 5);
 
 	private final String urlWithExternalJs = getResource("withExternalJs.html");
 	private final String urlExternalJs = getResource("external.js");
-	private final JavaScriptError errorExternalJs = new JavaScriptError("document.notExisting is undefined", urlExternalJs, 1);
+	private final JavaScriptError errorExternalJs = new JavaScriptError("TypeError: document.notExisting is undefined", urlExternalJs, 1);
 
 	/**
 	 *
